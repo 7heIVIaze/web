3fd4be9a0e6bdeeafb1c35f332b4fbbdcb799dfc
7heIVIaze <kimdave20@naver.com>
2022-06-05 22:28:41 +0900

    test

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
       <v-divider />
 
       <v-flex>
-        <div style="text-align: center;"><h1>sizing & self - produced</h1></div>
+        <div style="text-align: center; padding: 10px"><h1>sizing & self - produced</h1></div>
         <v-row>
           <v-col v-for="main in mains" :key="main.id" cols="3">
             <v-card hover :activator="{ on }">
@@ -49,124 +49,6 @@
           </v-col>
         </v-row>
       </v-flex>
-
-      <!-- <v-flex>
-        <div style="text-align: center;"><h1>sizing & self - produced</h1></div>
-        <v-row>
-          <v-col cols="3">
-            <v-card>
-              <v-img :src="main.profileUrl[0]" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-              <v-dialog v-model="DifferentSizeDialog" persistent max-width="600px">
-                <template v-slot:activator="{ on }">
-                  <v-img src="../static/storeImages/differ.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                </template>
-                <v-container>
-                  <v-card>
-                    <v-card-title><span class="headline">Different</span></v-card-title>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="DifferentSizeDialogClose" icon><v-icon>mdi-close</v-icon></v-btn>
-                    <v-card-text>
-                      <v-col>
-                        <v-row>
-                          <v-img src="../static/storeImages/differ.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                
-                        </v-row>
-                      </v-col>
-                      <v-col>
-                        <v-row>
-                          <span><i>People have different body sizes.</i></span>
-                        </v-row>
-                      </v-col>
-                    </v-card-text>
-                  </v-card>
-                </v-container>
-              </v-dialog>
-            </v-card>
-
-            <v-card>
-              <v-img src="../static/storeImages/size.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-              <v-dialog v-model="ClothSizeDialog" persistent max-width="600px">
-                <template v-slot:activator="{ on }">
-                  <v-img src="../static/storeImages/size.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                </template>
-                <v-container>
-                  <v-card>
-                    <v-card-title><span class="headline">Cloth Size</span></v-card-title>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="ClothSizeDialogClose" icon><v-icon>mdi-close</v-icon></v-btn>
-                    <v-card-text>
-                      <v-col>
-                        <v-row>
-                          <v-img src="../static/storeImages/size.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                        </v-row>
-                      </v-col>
-                      <v-col>
-                        <v-row>
-                          <span><i>But clothes are usually sold in standard sizes.</i></span>
-                        </v-row>
-                      </v-col>
-                    </v-card-text>
-                  </v-card>
-                </v-container>
-              </v-dialog>
-            </v-card>
-
-             <v-card>
-              <v-dialog v-model="MeasureDialog" persistent max-width="600px">
-                <template v-slot:activator="{ on }">
-                  <v-img src="../static/storeImages/measure.png" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                </template>
-                <v-container>
-                  <v-card>
-                    <v-card-title><span class="headline">Sizing</span></v-card-title>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="MeasureDialogClose" icon><v-icon>mdi-close</v-icon></v-btn>
-                    <v-card-text>
-                      <v-col>
-                        <v-row>
-                          <v-img src="../static/storeImages/measure.png" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                        </v-row>
-                      </v-col>
-                      <v-col>
-                        <v-row>
-                          <span><i>We make pants according to different body sizes for each customer.</i></span>
-                        </v-row>
-                      </v-col>
-                    </v-card-text>
-                  </v-card>
-                </v-container>
-              </v-dialog>
-            </v-card>
-
-             <v-card>
-              <v-dialog v-model="MendingDialog" persistent max-width="600px">
-                <template v-slot:activator="{ on }">
-                  <v-img src="../static/storeImages/mending.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                </template>
-                <v-container>
-                  <v-card>
-                    <v-card-title><span class="headline">Making</span></v-card-title>
-                    <v-spacer></v-spacer>
-                    <v-btn @click="MendingDialogClose" icon><v-icon>mdi-close</v-icon></v-btn>
-                    <v-card-text>
-                      <v-col>
-                        <v-row>
-                          <v-img src="../static/storeImages/mending.jpg" class="white--text" height="200px" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"></v-img>
-                        </v-row>
-                      </v-col>
-                      <v-col>
-                        <v-row>
-                          <span><i>Our products are made by craftsmen according to the customer's size.</i></span>
-                        </v-row>
-                      </v-col>
-                    </v-card-text>
-                  </v-card>
-                </v-container>
-              </v-dialog>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-flex> -->
 
     </v-flex>
   </v-layout>
